4b058a62f49430b57670837d7c72c894ea9a1136
Ceyvion <ceyvion@users.noreply.github.com>
2025-11-09 04:20:59 -0500

    Point Craft API to graphql actions

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 const CRAFT_GRAPHQL_URL =
-  process.env.CRAFT_GRAPHQL_URL || 'https://afropop.org/api'
+  process.env.CRAFT_GRAPHQL_URL || 'https://afropop.org/actions/graphql/api'
 const CRAFT_API_TOKEN =
   process.env.CRAFT_API_TOKEN || 'eak0Xj8_E_uiijiJRozmWzIn2hiIyCbS'
 
